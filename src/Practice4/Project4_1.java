f9cdd33a151ca06628c893fb78a35605815db0ff
Manatsu Shoji <egeg.mmm@gmail.com>
2017-05-11 00:47:54 +0900

    commit_0509

---
+++ src/Practice4/Project4_1.java
@@ -0,0 +1,25 @@
+package Practice4;
+
+public class Project4_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		/*
+		 * 練習問題 4 - 1
+         * “SPAM”という単語を 10 回表示するプログラムを作成しなさい。
+		 */
+		
+		// 変数宣言
+		String tango; // 単語
+
+		// 値代入
+		tango = "SPAM";
+
+		// 繰り返し
+		for (int kaisuu = 0; kaisuu <= 10; kaisuu++) {
+			System.out.println(tango);
+		}
+	}
+
+}
---
+++ src/Practice4/Project4_2.java
@@ -0,0 +1,17 @@
+package Practice4;
+
+public class Project4_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		/*
+		 * 練習問題 4 - 2 
+		 * 九九、三の段（ 3 ～ 27 の 3 の倍数）を表示するプログラムを作成しなさい。
+		 */
+
+		for (int x = 1; x <= 9; x++) {
+			System.out.println(x+"*3="+ 3 * x);
+		}
+	}
+
+}
